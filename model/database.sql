6ae2750548100d72d9533df9ae3372fb56c1f062
zsaidov1988 <zsaidov1988@gmail.com>
2022-03-16 15:01:32 +0500

    assas

--- model/database.sql
+++ model/database.sql
@@ -1,18 +1,20 @@
-create TABLE users(
+create database library;
+
+create TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   login VARCHAR(255),
   password VARCHAR(255),
   role VARCHAR(255),
   books VARCHAR(255)
-)
+);
 
-create TABLE authors(
+create TABLE IF NOT EXISTS authors(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
 
-create TABLE books(
+create TABLE IF NOT EXISTS books(
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   authorId INTEGER,
